4b1dea365e4fb7da442b3f376aa1fb9a3178dc3e
JoaoZati <joaozati@gmail.com>
2021-10-14 19:39:23 -0300

    breach backtesting: commit 14: - initiating backtesting_numba function;

--- backtesting_numba/backtesting.py
+++ backtesting_numba/backtesting.py
@@ -1,6 +1,31 @@
 from backtesting_numba.data_class import DataClass
-# import numpy as np
+import numpy as np
+from numba import njit
+
+
 # import pandas as pd
+
+
+@njit(parallel=True)
+def backtesting_numba(
+        op, hi, lo, cl,
+        be, bc,
+        se, sc,
+        c_enter, c_exit, s_enter, s_exit,
+        btl, bsl_atr, bsl_value,
+        btp, btp_atr, btp_value,
+        bts, bts_atr, bts_value,
+        ssl, ssl_atr, ssl_value,
+        stp, stp_atr, stp_value,
+        sts, sts_atr, sts_value,
+        revert, signal,
+        bc_last, sc_last,
+):
+
+    short_long, enter_price, exit_price, stop_loss_level, take_profit_level, traling_stop_level = [np.zeros(len(op))]*6
+    stop_loss, take_profit, trailing_stop = [0]*3
+
+    return short_long, enter_price, exit_price, stop_loss_level, take_profit_level, traling_stop_level
 
 
 class Backtesting:
@@ -54,3 +79,45 @@ class Backtesting:
             self.data_class._set_sell_close(sell_close)
         except Exception as e:
             raise e
+
+    def backtesting(
+            self,
+            comission_enter=0, comission_exit=0, slippage_enter=0, slippage_exit=0,
+            buy_stop_loss=False, bsl_atr=True, bsl_value=2,
+            buy_take_profit=False, btp_atr=True, btp_value=2,
+            buy_trailing_stop=False, bts_atr=True, bts_value=2,
+            sell_stop_loss=False, ssl_atr=True, ssl_value=2,
+            sell_take_profit=False, stp_atr=True, stp_value=2,
+            sell_trailing_stop=False, sts_atr=True, sts_value=2,
+            revert=False, signal=0,
+            buy_close_last=False, sell_close_last=False,
+    ):
+        len_data = len(self.data_class.dataframe)
+
+        if self.data_class.buy_enter is None:
+            self.data_class.buy_enter = np.zeros(len_data)
+
+        if self.data_class.buy_close is None:
+            self.data_class.buy_close = np.zeros(len_data)
+
+        if self.data_class.sell_enter is None:
+            self.data_class.sell_enter = np.zeros(len_data)
+
+        if self.data_class.sell_close is None:
+            self.data_class.sell_close = np.zeros(len_data)
+
+        short_long, enter_price, exit_price, stop_loss_level, take_profit_level, traling_stop_level = \
+            backtesting_numba(
+                self.data_class.open, self.data_class.high, self.data_class.low, self.data_class.close,
+                self.data_class.buy_enter, self.data_class.buy_close,
+                self.data_class.sell_enter, self.data_class.sell_close,
+                comission_enter, comission_exit, slippage_enter, slippage_exit,
+                buy_stop_loss, bsl_atr, bsl_value,
+                buy_take_profit, btp_atr, btp_value,
+                buy_trailing_stop, bts_atr, bts_value,
+                sell_stop_loss, ssl_atr, ssl_value,
+                sell_take_profit, stp_atr, stp_value,
+                sell_trailing_stop, sts_atr, sts_value,
+                revert, signal,
+                buy_close_last, sell_close_last,
+            )
